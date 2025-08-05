00304e198ceaf359c14411ad815db985ca8f8975
Noor Alam <nooralam192001@gmail.com>
2025-08-05 15:42:33 +0530

    remove signup and signin pages

--- src/App.jsx
+++ src/App.jsx
@@ -32,9 +32,9 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<SignUp />} />
+      {/* <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
-      <Route path="/verify" element={<Verify/>}  />
+      <Route path="/verify" element={<Verify/>}  /> */}
     </Routes>
   );
 }
